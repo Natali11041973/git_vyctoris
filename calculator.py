689bf10b7ee8610685876a951f9cc4b77aceac57
Natali <kazarenko.natali@mail.ru>
2025-05-05 00:48:27 +0300

    Task5 - функция деления реализована

--- calculator.py
+++ calculator.py
@@ -1,9 +1,17 @@
-
+import traceback
 class Calculator:
     last_res = None
     def sum(self, n1, n2):
         self.last_res = n1 + n2
         return n1 + n2
+    def divide(self, n1,n2):
+        try:
+            res = n1 / n2
+            self.last_res = res
+            return res
+        except:
+            traceback.print_exc()
+
 
     def multiply(self, n1, n2):
         self.last_res = n1 * n2
--- test_calculator.py
+++ test_calculator.py
@@ -4,4 +4,5 @@ from calculator import Calculator
 calc = Calculator()
 calc.sum(18, 5)
 calc.multiply(18, 5)
+calc.divide(18, 5)
 calc.print_last_res()
